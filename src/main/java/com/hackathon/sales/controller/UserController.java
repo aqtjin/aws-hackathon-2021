be8615c9f30809c83abaa218c9b300d635e5cbf1
aqtjin <aqtjin@outlook.com>
2021-04-18 22:27:01 +0800

    token 分布式会话

--- src/main/java/com/hackathon/sales/controller/UserController.java
+++ src/main/java/com/hackathon/sales/controller/UserController.java
@@ -11,6 +11,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.tomcat.util.security.MD5Encoder;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,25 +25,25 @@ import java.net.BindException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 @Controller("user")
 @RequestMapping("/user")
 @CrossOrigin(originPatterns = "*", allowCredentials="true", allowedHeaders="*")
 public class UserController extends BaseController {
 
-
-
     private final UserService userService;
 
     private final HttpServletRequest httpServletRequest;
 
+    private RedisTemplate redisTemplate;
+
     @Autowired
-    public UserController(UserService userService, HttpServletRequest httpServletRequest) {
+    public UserController(UserService userService, HttpServletRequest httpServletRequest, RedisTemplate redisTemplate) {
         this.userService = userService;
         this.httpServletRequest = httpServletRequest;
+        this.redisTemplate = redisTemplate;
     }
 
     //用户登录接口
@@ -59,8 +60,18 @@ public class UserController extends BaseController {
         UserModel userModel = userService.validateLogin(telephone, DigestUtils.md5DigestAsHex(password.getBytes(StandardCharsets.UTF_8)));
 
         //将登录凭证加入到用户登录成功的session内
-        this.httpServletRequest.getSession().setAttribute("IS_LOGIN", true);
-        this.httpServletRequest.getSession().setAttribute("LOGIN_USER", userModel);
+        //修改成若用户登录验证成功后将对应的登录信息和登录凭证一起存入redis中
+
+        //生成登录凭证token，UUID
+        String uuidToken = UUID.randomUUID().toString();
+        uuidToken = uuidToken.replace("-","");
+        //建立token和用户登录态之间的联系
+        redisTemplate.opsForValue().set(uuidToken,userModel);
+        redisTemplate.expire(uuidToken,1, TimeUnit.HOURS);
+
+
+//        this.httpServletRequest.getSession().setAttribute("IS_LOGIN", true);
+//        this.httpServletRequest.getSession().setAttribute("LOGIN_USER", userModel);
 
         return CommonReturnType.create(null);
     }
