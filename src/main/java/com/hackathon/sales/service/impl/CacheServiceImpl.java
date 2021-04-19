063a886af23f59a9f6f7c92ab7527051cb4d0b57
aqtjin <aqtjin@outlook.com>
2021-04-19 13:04:12 +0800

    redis缓存与本地缓存，以及商品列表页生成促销活动

---
+++ src/main/java/com/hackathon/sales/service/impl/CacheServiceImpl.java
@@ -0,0 +1,36 @@
+package com.hackathon.sales.service.impl;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.hackathon.sales.service.CacheService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class CacheServiceImpl implements CacheService {
+
+    private Cache<String, Object> commonCache = null;
+
+    @PostConstruct
+    public void init() {
+        commonCache = CacheBuilder.newBuilder()
+                //设置缓存容器的初始容量为10
+                .initialCapacity(10)
+                //设置缓存中最大可以存储100个key，超过100个以后会按照LRU的策略移除缓存项
+                .maximumSize(100)
+                //设置写缓存后60秒过期
+                .expireAfterWrite(60, TimeUnit.SECONDS).build();
+    }
+
+    @Override
+    public void setCommonCache(String key, Object value) {
+        commonCache.put(key, value);
+    }
+
+    @Override
+    public Object getCommonCache(String key) {
+        return commonCache.getIfPresent(key);
+    }
+}
